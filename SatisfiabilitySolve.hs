7ba6d809599a27659c491572e22317060877177d
Fuad Saud <fuadfsaud@gmail.com>
2015-04-03 03:49:58 -0400

    Try to get sat solver to work

--- SatisfiabilitySolve.hs
+++ SatisfiabilitySolve.hs
@@ -1,21 +1,35 @@
 module SatisfiabilitySolve where
 
-data BExp = BConst Bool | Var String | And BExp BExp | Or BExp BExp | Not BExp deriving (Eq,Show)
+import Solver
+import Data.Map (Map)
+import qualified Data.Map as Map
 
-data SatConfig = SatConfig deriving Eq
+data BExp = BConst Bool | Var String | And BExp BExp | Or BExp BExp | Not BExp deriving (Eq)
+
+instance Show BExp where
+    show (BConst value)  = show value
+    show (Var varName)   = varName
+    show (And lexp rexp) = "(" ++ (show lexp) ++ " && " ++ (show rexp) ++ ")"
+    show (Or  lexp rexp) = "(" ++ (show lexp) ++ " || " ++ (show rexp) ++ ")"
+    show (Not exp)       = '~':(show exp)
+
+data SatConfig = SatConfig BExp (Map String Bool) deriving Eq
 
 instance Show SatConfig where
-    show (SatConfig (BConst value))  = show value
-    show (SatConfig (Var varName))   = varName
-    show (SatConfig (And lexp rexp)) = (show lexp) ++ "||" ++ (show rexp)
-    show (SatConfig (Or  lexp rexp)) = (show lexp) ++ "&&" ++ (show rexp)
-    show (SatConfig (Not exp))       = '~':(show exp)
+  show (SatConfig bexp) = show bexp
 
 instance Config SatConfig where
-    successors = []
+    successors config = []
 
-isGoal :: SatConfig -> Bool
-isGoal _ = False
+isSatisfiable :: SatConfig -> Bool
+isSatisfiable (SatConfig (BConst True))   = True
+isSatisfiable (SatConfig (BConst False))  = False
+isSatisfiable (SatConfig (Var _))         = True
+isSatisfiable (SatConfig (And lexp rexp)) = isSatisfiable (SatConfig lexp) && isSatisfiable (SatConfig rexp)
+isSatisfiable (SatConfig (Or lexp rexp))  = isSatisfiable (SatConfig lexp) || isSatisfiable (SatConfig rexp)
+isSatisfiable (SatConfig (Not exp))       = isSatisfiable (SatConfig exp)
 
 satSolve :: BExp -> Maybe SatConfig
-satSolve bexp = solve isGoal (SatConfig bexp)
+satSolve bexp = solve isSatisfiable initialConfig
+  where
+    initialConfig = SatConfig bexp (Map.fromList [])
